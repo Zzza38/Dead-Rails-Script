5886ad771e20ffed539e01996adac4f4bda9b1f2
Zion Aronov <zwatch3@gmail.com>
2025-05-02 18:57:59 -0400

    using rayfield instead

--- src/script.lua
+++ src/script.lua
@@ -1,84 +1,117 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Zzza38/Dead-Rails-Script/main/src/OrionLib.lua')))()
-local Window = OrionLib:MakeWindow(
-	{
-		Name = "Dead Rails",
-		HidePremium = false,
-		SaveConfig = true,
-		ConfigFolder =
-		"dead-rails-script"
-	})
-local tabs = {}
-tabs.general = Window:MakeTab({
-	Name = "General Settings",
-	Icon = "rbxassetid://6774409980",
-	PremiumOnly = false
-})
-tabs.esp = Window:MakeTab({
-	Name = "ESP",
-	Icon = "rbxassetid://77626488648237",
-	PremiumOnly = false
+-- Load Rayfield library
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+-- Create the main window
+local Window = Rayfield:CreateWindow({
+    Name = "Dead Rails",
+    LoadingTitle = "Loading...",
+    LoadingSubtitle = "Please wait",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "dead-rails-script",
+        FileName = "config"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "",
+        RememberJoins = true
+    },
+    KeySystem = false,
+    KeySettings = {
+        Title = "Access Key",
+        Subtitle = "Enter the key to continue",
+        Note = "This is a custom key system",
+        FileName = "KeyConfig",
+        SaveKey = true
+    }
 })
 
-tabs.general:AddButton({
-	Name = "Button!",
-	Callback = function()
-		print("button pressed")
-	end
+-- Create tabs
+local generalTab = Window:CreateTab("General Settings", 4483362458)
+local espTab = Window:CreateTab("ESP", 4483362458)
+
+-- Add general settings section
+local generalSection = generalTab:CreateSection("Main Settings", true)
+local espSection = espTab:CreateSection("ESP Options", true)
+
+-- Button example
+generalTab:CreateButton({
+    Name = "Button!",
+    Callback = function()
+        print("Button pressed")
+    end
 })
 
-tabs.general:AddToggle({
-	Name = "This is a toggle!",
-	Default = false,
-	Callback = function(Value)
-		print(Value)
-	end
+-- Toggle examplea
+generalTab:CreateToggle({
+    Name = "This is a toggle!",
+    Default = false,
+    Callback = function(Value)
+        print(Value)
+    end
 })
-tabs.general:AddColorpicker({
-	Name = "Colorpicker",
-	Default = Color3.fromRGB(255, 0, 0),
-	Callback = function(Value)
-		print(Value)
-	end
+
+-- Colorpicker example
+generalTab:CreateColorPicker({
+    Name = "Color Picker",
+    Default = Color3.fromRGB(255, 0, 0),
+    Callback = function(Value)
+        print(Value)
+    end
 })
-local Slider = tabs.general:AddSlider({
-	Name = "Slider",
-	Min = 16,
-	Max = 100,
-	Default = 16,
-	Color = Color3.fromRGB(255, 255, 255),
-	Increment = 1,
-	ValueName = "bananas",
-	Callback = function(Value)
-		print(Value)
-	end
+
+-- Slider example
+local Slider = generalTab:CreateSlider({
+    Name = "Slider",
+    Min = 16,
+    Max = 100,
+    Default = 16,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "bananas",
+    Callback = function(Value)
+        print(Value)
+    end
 })
 Slider:Set(18)
-local CoolLabel = tabs.general:AddLabel("Label")
+
+-- Label example
+local CoolLabel = generalTab:CreateLabel("Label")
 CoolLabel:Set("Label New!")
-local CoolParagraph = tabs.general:AddParagraph("Paragraph", "Paragraph Content")
+
+-- Paragraph example
+local CoolParagraph = generalTab:CreateParagraph("Paragraph", "Paragraph Content")
 CoolParagraph:Set("Paragraph New!", "New Paragraph Content!")
-tabs.general:AddTextbox({
-	Name = "Textbox",
-	Default = "default box input",
-	TextDisappear = true,
-	Callback = function(Value)
-		print(Value)
-	end
+
+-- Textbox example
+generalTab:CreateTextbox({
+    Name = "Textbox",
+    Default = "default box input",
+    TextDisappear = true,
+    Callback = function(Value)
+        print(Value)
+    end
 })
-tabs.general:AddBind({
-	Name = "Bind",
-	Default = Enum.KeyCode.E,
-	Hold = false,
-	Callback = function()
-		print("press")
-	end
+
+-- Bind example
+generalTab:CreateBind({
+    Name = "Bind",
+    Default = Enum.KeyCode.E,
+    Hold = false,
+    Callback = function()
+        print("Key pressed")
+    end
 })
-tabs.general:AddDropdown({
-	Name = "Dropdown",
-	Default = "1",
-	Options = { "1", "2" },
-	Callback = function(Value)
-		print(Value)
-	end
+
+-- Dropdown example
+generalTab:CreateDropdown({
+    Name = "Dropdown",
+    Default = "1",
+    Options = { "1", "2" },
+    Callback = function(Value)
+        print(Value)
+    end
 })
-OrionLib:Init()
+
+-- Final Rayfield Initialization
+Rayfield:LoadConfiguration()
